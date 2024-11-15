cb2bf704b408534d6de192af2cd9b706107acab5
hushengte <944576192@qq.com>
2024-11-15 18:17:40 +0800

    polish iam service configuration

--- disciples-iam-core/src/main/java/com/disciples/iam/config/ServiceConfiguration.java
+++ disciples-iam-core/src/main/java/com/disciples/iam/config/ServiceConfiguration.java
@@ -4,19 +4,26 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.core.convert.JdbcConverter;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.data.relational.core.mapping.RelationalMappingContext;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.disciples.data.jdbc.repository.config.MybatisSupportConfiguration;
+import com.disciples.data.jdbc.repository.support.MybatisRepositoryFactoryBean;
 import com.disciples.iam.identity.GroupManager;
 import com.disciples.iam.identity.IamUserDetailsService;
+import com.disciples.iam.identity.IdentityQueryService;
 import com.disciples.iam.identity.UserManager;
 import com.disciples.iam.identity.domain.Groups;
 import com.disciples.iam.identity.domain.Users;
 import com.disciples.iam.util.Md5PasswordEncoder;
 
 @Configuration(proxyBeanMethods = false)
-public class ServiceConfiguration {
+@EnableJdbcRepositories(basePackages = {"com.disciples.iam.identity.domain"}, repositoryFactoryBeanClass = MybatisRepositoryFactoryBean.class)
+public class ServiceConfiguration extends MybatisSupportConfiguration {
     
 	@Bean
     public JdbcOperations jdbcTemplate(DataSource dataSource) {
@@ -31,6 +38,12 @@ public class ServiceConfiguration {
 	@Bean
 	public GroupManager groupManager(Groups groups) {
 		return new GroupManager(groups);
+	}
+	
+	@Bean
+	public IdentityQueryService identityQueryService(JdbcOperations jdbcOperations, Groups groups,
+			RelationalMappingContext mappingContext, JdbcConverter jdbcConverter) {
+		return new IdentityQueryService(jdbcOperations, groups, mappingContext, jdbcConverter);
 	}
 	
 	@Bean
--- disciples-iam-core/src/test/java/com/disciples/iam/config/ManagerConfig.java
+++ disciples-iam-core/src/test/java/com/disciples/iam/config/ManagerConfig.java
@@ -1,20 +1,14 @@
 package com.disciples.iam.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
-import com.disciples.data.jdbc.repository.config.MybatisSupportConfiguration;
-import com.disciples.data.jdbc.repository.support.MybatisRepositoryFactoryBean;
 import com.disciples.iam.annotation.EnableIam;
 
 @Configuration
 @Import({DbConfig.class})
 @EnableIam
-@ComponentScan(basePackages = {"com.disciples.iam.identity"})
-@EnableJdbcRepositories(basePackages = {"com.disciples.iam.identity.domain"}, repositoryFactoryBeanClass = MybatisRepositoryFactoryBean.class)
 //@EnableJdbcAuditing
-public class ManagerConfig extends MybatisSupportConfiguration {
+public class ManagerConfig {
 	
 }
